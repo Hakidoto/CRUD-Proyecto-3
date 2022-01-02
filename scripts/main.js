57854f3958918e8509157de89255d4c51a394d4d
Hadid Garcia <hadid.garcia1@gmail.com>
2022-01-02 17:49:38 -0600

    Conversion del almacenado e insertado a localstorage
    Signed-off-by: Hadid Garcia <hadid.garcia1@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 var Fila = null;
+var ArrayUsuarios = [];
 
 function onSubmit(){
     let DataForm = LeerDatos()
     if(Fila == null){
-        InsertarDatos(DataForm)
+        AlmacenarDatos(DataForm)
     } else{
         ActualizarDatos(DataForm)
         VaciarDatos()
@@ -19,20 +20,53 @@ DataForm["aMat"] = document.getElementById("aMat").value
 DataForm["direc"] = document.getElementById("direc").value
 DataForm["email"] = document.getElementById("email").value
 DataForm["ciudad"] = document.getElementById("ciudad").value
+
 return DataForm
 }
 
-function InsertarDatos(data) {
+function AlmacenarDatos(DataForm){
+   // ArrayUsuarios.push(DataForm)
+   //localStorage.setItem("DatosUsuarios", JSON.stringify(ArrayUsuarios))
+   let ListaUsuario = {
+       "nombre" : DataForm.nombre,
+       "aPat" : DataForm.aPat,
+       "aMat" : DataForm.aMat,
+       "direc" : DataForm.direc,
+       "email" : DataForm.email,
+       "ciudad" : DataForm.ciudad
+   };
+   if(localStorage.getItem('ArrayUsuarios') == null) {
+       localStorage.setItem('ArrayUsuarios', '[]');
+   }
+
+   let usuarios = JSON.parse(localStorage.getItem('ArrayUsuarios'))
+   usuarios.push(ListaUsuario);
+
+   localStorage.setItem('ArrayUsuarios', JSON.stringify(usuarios));
+   if(localStorage.getItem('ArrayUsuarios') != null) {
+       console.log(JSON.parse(localStorage.getItem('ArrayUsuarios')));
+   }
+
+   InsertarDatos()
+}
+
+function InsertarDatos() {
 let table = document.getElementById("tabla").getElementsByTagName('tbody')[0]
 let Fila = table.insertRow(table.length)
-columna1 = Fila.insertCell(0).innerHTML = data.nombre
-columna2 = Fila.insertCell(1).innerHTML = data.aPat
-columna3 = Fila.insertCell(2).innerHTML = data.aMat
-columna4 = Fila.insertCell(3).innerHTML = data.direc
-columna5 = Fila.insertCell(4).innerHTML = data.email
-columna6 = Fila.insertCell(5).innerHTML = data.ciudad
-columna6 = Fila.insertCell(6).innerHTML = `<input class="submit" type="button" onClick="EditarDatos(this)" value="Editar">
+var ListaCompleta = JSON.parse(localStorage.getItem("ArrayUsuarios"));
+    console.log(ListaCompleta);
+
+for(var i=1 in ListaCompleta){
+    columna1 = Fila.insertCell(0).innerHTML = ListaCompleta[i].nombre
+    columna2 = Fila.insertCell(1).innerHTML = ListaCompleta[i].aPat
+    columna3 = Fila.insertCell(2).innerHTML = ListaCompleta[i].aMat
+    columna4 = Fila.insertCell(3).innerHTML = ListaCompleta[i].direc
+    columna5 = Fila.insertCell(4).innerHTML = ListaCompleta[i].email
+    columna6 = Fila.insertCell(5).innerHTML = ListaCompleta[i].ciudad
+    columna6 = Fila.insertCell(6).innerHTML = `<input class="submit" type="button" onClick="EditarDatos(this)" value="Editar">
                                             <input class="submit" type="button" onClick="BorrarDatos(this)" value="Borrar">`
+}
+
 document.getElementById("nombre").focus
 
 }
